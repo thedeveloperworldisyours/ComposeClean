0f129a416fd59aecdf82156b768e6646a341c2a6
Cabezas <javiergonzalezcabezas@gmail.com>
2022-01-19 18:50:06 +0100

    deleted Constants

--- app/src/main/java/com/a/vocabulary15/presentation/common/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.a.vocabulary15.presentation.common
-
-object Constants {
-    const val EXPAND_ANIMATION = 300
-    const val COLLAPSE_ANIMATION = 300
-    const val FADE_IN_ANIMATION = 300
-    const val FADE_OUT_ANIMATION = 300
-}
--- app/src/main/java/com/a/vocabulary15/presentation/element/ElementsViewModel.kt
+++ app/src/main/java/com/a/vocabulary15/presentation/element/ElementsViewModel.kt
@@ -1,6 +1,5 @@
 package com.a.vocabulary15.presentation.element
 
-import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue

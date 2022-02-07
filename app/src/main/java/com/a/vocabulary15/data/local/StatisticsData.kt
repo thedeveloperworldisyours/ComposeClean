62b63574563e9851eeffff73f246bda9fd26f789
Cabezas <javiergonzalezcabezas@gmail.com>
2022-02-07 21:05:20 +0100

    added hangman images

--- app/src/main/java/com/a/vocabulary15/data/local/StatisticsData.kt
+++ app/src/main/java/com/a/vocabulary15/data/local/StatisticsData.kt
@@ -1,8 +1,10 @@
 package com.a.vocabulary15.data.local
 
 import androidx.room.ColumnInfo
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity
 data class StatisticsData(
     @PrimaryKey(autoGenerate = true) var id: Int,
     @ColumnInfo(name = "date") var date: Long = 0,
